57d59d069e65f2c1f33335f10ebf9065be197650
Mihai Hadirca <hadircamihai98@gmail.com>
2025-07-18 14:14:41 -0400

    ok nvm now its good

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
-
 
 app.use(express.static('static'));
 
-
-app.listen(port, () => {
-    console.log('listening on port http://localhost:${port}')
-})
+app.listen(3000, 'localhost', () => {
+  console.log('listening on http://localhost:3000');
+});
